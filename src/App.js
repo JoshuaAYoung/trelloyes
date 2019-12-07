13acdb3859886f360869ea2e5cf96e2aba798fbc
JoshuaAYoung <joshua@young.net>
2019-12-07 12:50:13 -0700

    started app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Card from 'card';
+import List from 'list';
 
-function App() {
+
+function App(props) {
+  const listItem = STORE.lists.map(List);
   return (
-    <main className='App'>
-      {/* content goes here */}
+    <main class="App">
+      <header class="App-header">
+        <h1>Trelloyes!</h1>
+      </header>
+      <div class="App-list">
+        
+      </div>
     </main>
   );
 }
